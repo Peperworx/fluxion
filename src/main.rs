e030bf414b7ac52c5131b121821715362afee635
wireboy5 <riley.j.wilton@gmail.com>
2023-06-05 23:20:39 -0400

    Need a way to dynamically call into event handler.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 
+use std::any::{Any, self};
+
 use async_trait::async_trait;
-use fluxion::actor::{Actor, ActorMessage, ActorError, Context};
+use fluxion::actor::{Actor, ActorMessage, ActorError, Context, Handler, SystemEvent};
 
 struct TestMessage;
 
@@ -33,7 +35,28 @@ impl Actor for TestActor {
     }
 }
 
+#[async_trait]
+impl Handler<u32> for TestActor {
+    async fn event(&mut self, _context: Context, event: u32) -> Result<(), ActorError> {
+        println!("Recieved u32 {event}");
+        Ok(())
+    }
+}
+
+#[async_trait]
+impl Handler<i32> for TestActor {
+    async fn event(&mut self, _context: Context, event: i32) -> Result<(), ActorError> {
+        println!("Recieved u32 {event}");
+        Ok(())
+    }
+}
+
+async fn run_handle<T: Any + Send + Sync, A: Actor + Handler<T>>(actor: &mut A, context: Context, event: T) -> Result<(), ActorError> {
+    <A as Handler<T>>::event(actor, context, event).await
+}
+
 #[tokio::main]
 async fn main() {
-
+    let mut a1 = TestActor {};
+    let ctx = Context {};
 }
