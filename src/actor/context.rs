af68bb3d4e28fc5b231cd6aad043447e77f47475
Riley Wilton <riley.j.wilton@gmail.com>
2023-07-26 19:03:02 -0400

    Added tracing to context. Fixed readme.

--- src/actor/context.rs
+++ src/actor/context.rs
@@ -5,6 +5,9 @@ use crate::{
     system::{System, GetActorReturn},
 };
 
+#[cfg(feature = "tracing")]
+use tracing::{event, Level};
+
 use super::ActorID;
 
 /// # ActorContext
@@ -21,7 +24,12 @@ pub struct ActorContext<F: Message, N: Notification> {
 impl<F: Message, N: Notification> ActorContext<F, N> {
     /// Retrieves an actor from the system.
     /// Returns [`None`] if the actor does not exist or if an actor tries to retrieve its own handle.
+    #[cfg_attr(feature = "tracing", tracing::instrument(skip(self)))]
     pub async fn get_actor<M: Message>(&self, id: &str) -> Option<GetActorReturn<F, M>> {
+
+        #[cfg(feature = "tracing")]
+        event!(Level::TRACE, actor=self.id.to_string(), "Retrieving a handle to {} from an actor context", id);
+
         #[cfg(not(feature = "foreign"))]
         let new_id = id.to_string();
         #[cfg(feature = "foreign")]
@@ -29,6 +37,9 @@ impl<F: Message, N: Notification> ActorContext<F, N> {
 
         // If the the id matches the actor's own path, then return None
         if Some(&new_id) == Some(&self.id) {
+            #[cfg(feature = "tracing")]
+            event!(Level::TRACE, actor=self.id.to_string(), "Actor attempted to retrieve its own handle.");
+
             None
         } else {
             self.system.get_actor(id).await
