122cb2982d71097d5e44ad0afdad94c5596e9de0
Riley Wilton <30930649+wireboy5@users.noreply.github.com>
2023-08-31 17:12:36 +0000

    An option is to have all messages converted to &dyn Any and downcast to a trait. See example in main.rs.

--- src/main.rs
+++ src/main.rs
@@ -1,102 +1,40 @@
 #![cfg_attr(feature = "nightly", feature(async_fn_in_trait))]
 
-use std::marker::PhantomData;
+use std::any::Any;
 
-use fluxion::{
-    actor::{
-        supervisor::{ActorSupervisor, SupervisorGenerics},
-        Actor, ActorContext, Handle,
-    },
-    error::FluxionError,
-    message::{Message, MessageSerializer},
-};
-use serde::{Deserialize, Serialize};
-
-struct TestNotification;
-
-impl Message for TestNotification {
-    type Response = ();
+trait SomeTrait: Any {
+    fn some_fn(&self);
 }
 
-#[derive(Serialize, Deserialize)]
-struct TestMessage;
+struct Test;
 
-impl Message for TestMessage {
-    type Response = ();
-}
-
-struct TestActor;
-
-#[async_trait::async_trait]
-impl Actor for TestActor {
-    type Error = ();
-}
-
-#[async_trait::async_trait]
-impl Handle<TestMessage> for TestActor {
-    async fn message(
-        &mut self,
-        _message: &TestMessage,
-        _context: &mut ActorContext,
-    ) -> Result<(), FluxionError<()>> {
-        println!("1");
-        Ok(())
+impl SomeTrait for Test {
+    fn some_fn(&self) {
+        println!("hello test");
     }
 }
 
-#[async_trait::async_trait]
-impl Handle<TestNotification> for TestActor {
-    async fn message(
-        &mut self,
-        _message: &TestNotification,
-        _context: &mut ActorContext,
-    ) -> Result<(), FluxionError<()>> {
-        println!("notification");
-        Ok(())
+struct Test2;
+
+impl SomeTrait for Test2 {
+    fn some_fn(&self) {
+        println!("hello test");
     }
 }
 
-struct BincodeSerializer;
+fn some_downcasted(value: &dyn Any) {
+    let downcasted: &Box<dyn SomeTrait> = value.downcast_ref().unwrap();
 
-impl MessageSerializer for BincodeSerializer {
-    fn deserialize<T: for<'a> serde::Deserialize<'a>, E>(
-        message: Vec<u8>,
-    ) -> Result<T, FluxionError<E>> {
-        bincode::deserialize(&message).or(Err(FluxionError::DeserializeError))
-    }
-
-    fn serialize<T: serde::Serialize, E>(message: T) -> Result<Vec<u8>, FluxionError<E>> {
-        bincode::serialize(&message).or(Err(FluxionError::SerializeError))
-    }
-}
-
-struct FluxionParams<A, M, N>(PhantomData<A>, PhantomData<M>, PhantomData<N>);
-
-impl<M: Message + Serialize + for<'a> Deserialize<'a>, A: Actor + Handle<M>, N: Message>
-    SupervisorGenerics for FluxionParams<A, M, N>
-{
-    type Actor = A;
-
-    type Serializer = BincodeSerializer;
-
-    type Foreign = M;
-
-    type Notification = N;
+    downcasted.some_fn();
 }
 
 #[tokio::main]
 async fn main() {
-    let actor = TestActor;
+    let test = Test2;
 
-    let (mut supervisor, actorref) =
-        ActorSupervisor::<FluxionParams<TestActor, TestMessage, TestNotification>>::new(actor);
+    let test: Box<dyn SomeTrait + 'static> = Box::new(test);
 
-    let jh = tokio::spawn(async move {
-        supervisor.run().await.unwrap();
-    });
+    let test_any: &dyn Any = &test;
 
-    actorref.request(TestMessage).await.unwrap();
-    actorref.request(TestNotification).await.unwrap();
-
-    jh.await.unwrap();
+    some_downcasted(test_any);
 }
--- src/system/mod.rs
+++ src/system/mod.rs
@@ -4,6 +4,7 @@
 /// # `System`
 /// The system contains all actors, and is responsible for dispatching messages to other actors.
 /// It is responsible for ticking all ActorSupervisors, which allow the user to chose an async executor.
+///
 pub struct System {}
 
 impl System {
