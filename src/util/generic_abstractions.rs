c520465810b49f9692abcb97ee549170393b9d99
Riley Wilton <riley.j.wilton@gmail.com>
2023-09-05 22:43:13 -0400

    Now comes an interesting predicament: where should errors be attached to? I am going to go with the system for now.

--- src/util/generic_abstractions.rs
+++ src/util/generic_abstractions.rs
@@ -3,16 +3,38 @@
 //! that uses associated types to store the values.
 //! This method significantly reduces repetition and makes code much easier to read.
 //!
-//! ## [`SystemParams`]
+//! ## [`MessageParams`]
 //! This is the basic generic abstraction. It does not depend on any other generic abstractions, and it contains
 //! only two associated types: `Federated` and `Notification`, which only exist if their respective feature flags are enabled.
 
 use crate::message::Message;
 
-/// # [`SystemParams`]
+/// # [`MessageParams`]
 /// The simplest generic abstraction, containing the federated message and notification types.
-pub trait SystemParams {
+pub trait MessageParams {
     /// The federated message associated with the system
     #[cfg(federated)]
     type Federated: Message;
+
+    /// The notification associated with the system
+    #[cfg(notification)]
+    type Notification: Message;
 }
+
+/// # [`ActorParams`]
+/// Parameters that are specific to a single actor. This includes the [`MessageParams`], the actor's [`Message`],
+/// and the error type that the actor can return.
+pub trait ActorParams {
+
+    /// The message that the actor can handle
+    type Message: Message;
+
+    /// The [`MessageParams`] that this actor needs to support.
+    /// This is named [`SystemMessages`] because it contains message types uniform
+    /// across the entire system
+    type SystemMessages: MessageParams;
+
+    /// The error type that this actor may return
+    
+}
+
