bc00c74cec2aff8388e34f2958cc6d8120eb9f36
Riley Wilton <riley.j.wilton@gmail.com>
2023-09-20 18:35:40 -0400

    Working on different crates.

---
+++ fluxion_actor/src/lib.rs
@@ -0,0 +1,74 @@
+#! [doc = include_str! ("../README.md")]
+
+
+
+// The following will be in *every* crate related to fluxion.
+#![no_std]
+#![cfg_attr(not(async_trait), feature(async_fn_in_trait))]
+#![warn(clippy::pedantic)]
+#![allow(clippy::module_name_repetitions)]
+
+
+/// # Actor
+/// This trait must be implemented for all Actors. It contains three functions, [`Actor::initialize`], [`Actor::deinitialize`], and [`Actor::cleanup`].
+/// Each have a default implementation which does nothing.
+///
+/// ## Initialization
+/// When an Actor is added to a system, a separate management, or "supervisor" task is started which oversees the Actor's lifetime.
+/// When this supervisor task is started, [`Actor::initialize`] is immediately called. If successful, the supervisor begins the Actor's
+/// main loop. Upon failure, the supervisor immediately skips to the cleanup phase.
+///
+/// ## Deinitialization
+/// If the actor's main loop exits, either gracefully or by an error, [`Actor::deinitialize`] is called. Reguardless of if this function
+/// fails or not, the actor skips to the cleanup phase.
+///
+/// ## Cleanup
+/// After the supervisor task exits, [`Actor::cleanup`] is called. In place of the ActorContext, an `Option<Self::Error>` is provided, containing None
+/// if the supervisor exited gracefully, or `Some(error)` if the supervisor task failed with an error. This function is always called on actor exit.
+/// If [`Actor::cleanup`] returns an error, the error is simply logged if tracing is enabled, and the actor stops.
+///
+/// ## Async
+/// This trait uses [`async_trait`] when on stable. Once async functions in traits are stablized, this dependency will be removed.
+/// On nightly, the `nightly` feature may be enabled, which uses `#![feature(async_fn_in_trait)]`
+#[cfg_attr(async_trait, async_trait::async_trait)]
+pub trait Actor: Send + Sync + 'static {
+    /// The error type returned by the actor
+    type Error: Send + Sync + 'static;
+
+    /// The function run upon actor initialization
+    async fn initialize(
+        &mut self,
+        _context: &ActorContext,
+    ) -> Result<(), ActorError<Self::Error>> {
+        Ok(())
+    }
+
+    /// The function run upon actor deinitialization
+    async fn deinitialize(
+        &mut self,
+        _context: &ActorContext,
+    ) -> Result<(), ActorError<Self::Error>> {
+        Ok(())
+    }
+
+    /// The function run upon actor cleanup
+    async fn cleanup(
+        &mut self,
+        _error: Option<ActorError<Self::Error>>,
+    ) -> Result<(), ActorError<Self::Error>> {
+        Ok(())
+    }
+}
+
+/// # Handle
+/// Actors may implement this trait to handle messages or notifications.
+/// Because the message handler may be spawned in a separate task, message handlers do not have mutable access to 
+/// the actor.
+#[cfg_attr(async_trait, async_trait::async_trait)]
+pub trait Handle<M: Message>: Actor {
+    async fn message(
+        &self,
+        message: &M,
+        _context: &ActorContext,
+    ) -> Result<M::Response, ActorError<Self::Error>>;
+}
--- fluxion_types/src/atg.rs
+++ fluxion_types/src/atg.rs
@@ -1,4 +1,78 @@
 //! # Associated Type Generics
-//! Fluxion uses associated types to pass around configuration and global types. The traits which implement those associated types are provided here,
-//! with their generic "converted" types provided in [`crate::param`]
+//! This file contains a way to abstract many different generics and feature flags into a singular, concise generic
+//! that uses associated types to store the values.
+//! This method significantly reduces repetition and makes code much easier to read.
+//!
+//! ## [`MessageParams`]
+//! This is the basic generic abstraction. It does not depend on any other generic abstractions, and it contains
+//! only two associated types: `Federated` and `Notification`, which only exist if their respective feature flags are enabled.
+//!
+//! ## [`ActorParams`]
+//! This contains both the contents of [`MessageParams`], a [`Message`] type, and the type of the actor that this parameter stores info for.
+//! This is used to provide generics for the actor's supervisor.
 
+
+use fluxion_message::Message;
+
+#[cfg(serde)]
+use crate::message::serializer::MessageSerializer;
+
+/// # [`MessageParams`]
+/// The simplest generic abstraction, containing the federated message and notification types.
+pub trait MessageParams: 'static {
+    /// The federated message associated with the system
+    #[cfg(federated)]
+    type Federated: Message;
+
+    /// The notification associated with the system
+    #[cfg(notification)]
+    type Notification: Message + Clone;
+}
+
+/// # [`SystemParams`]
+/// Parameters specific to an entire system, including the [`MessageParams`], and the [`MessageSerializer`]
+pub trait SystemParams: 'static {
+    /// The [`MessageParams`] associated with this system.
+    /// This is named [`SystemMessages`] because it contains message types uniform
+    /// across the entire system
+    #[cfg(any(federated, notification))]
+    type SystemMessages: MessageParams;
+
+    /// The [`MessageSerializer`] used for foreign messages across the system
+    #[cfg(serde)]
+    type Serializer: MessageSerializer;
+
+    /// The [`Executor`] that tasks are spawned on.
+    type Executor: Executor;
+}
+
+/// # [`ActorParams`]
+/// Parameters that are specific to a single actor. This includes the [`MessageParams`], and the actor's [`Message`] type.
+/// The [`SystemParams`] are provided as a generic to reduce long and complex associated type clairifications.
+pub trait ActorParams<S: SystemParams>: 'static {
+    /// The message that the actor can handle
+    type Message: Message;
+
+    cfg_if::cfg_if! {
+        if #[cfg(all(notification, federated))] {
+            /// The type of the actor itself.
+            type Actor: Actor
+                + Handle<Self::Message>
+                + Handle<<S::SystemMessages as MessageParams>::Notification>
+                + Handle<<S::SystemMessages as MessageParams>::Federated>;
+        } else if #[cfg(notification)] {
+            /// The type of the actor itself.
+            type Actor: Actor
+                + Handle<Self::Message>
+                + Handle<<S::SystemMessages as MessageParams>::Notification>;
+        } else if #[cfg(federated)] {
+            /// The type of the actor itself.
+            type Actor: Actor
+                + Handle<Self::Message>
+                + Handle<<S::SystemMessages as MessageParams>::Federated>;
+        } else {
+            /// The type of the actor itself.
+            type Actor: Actor + Handle<Self::Message>;
+        }
+    }
+}
