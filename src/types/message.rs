f2d698d5973acce986976f0b0f38853d4bfc064c
Riley Wilton <riley.j.wilton@gmail.com>
2023-09-25 22:02:18 -0400

    Handled trait done.

--- src/types/message.rs
+++ src/types/message.rs
@@ -1,9 +1,14 @@
 //! # Message Tyes
 //! Contains types and traits related to messages.
 
+// Needed by async_trait.
+#[cfg(async_trait)]
+use alloc::boxed::Box;
 
 #[cfg(serde)]
 use serde::{Deserialize, Serialize};
+
+use super::{actor::Actor, Handle, errors::ActorError};
 
 
 /// # Message
@@ -18,4 +23,23 @@ pub trait Message: Send + Sync + 'static {
 pub trait Message: Serialize + for<'a> Deserialize<'a> + Send + Sync + 'static {
     /// The message's response
     type Response: Serialize + for<'a> Deserialize<'a> + Send + Sync + 'static;
+}
+
+
+/// # Handled
+/// Not to be confused with [`Handle`], this is a trait implemented for every message for which an [`Actor`] implements [`Handle`].
+#[cfg_attr(async_trait, async_trait::async_trait)]
+pub trait Handled<A: Actor>: Message {
+    async fn handle(&self, actor: &A) -> Result<Self::Response, ActorError<A::Error>>;
+}
+
+#[cfg_attr(async_trait, async_trait::async_trait)]
+impl<T, A> Handled<A> for T
+where
+    T: Message,
+    A: Actor + Handle<T> {
+
+    async fn handle(&self, actor: &A) -> Result<Self::Response, ActorError<<A as Actor>::Error>> {
+        actor.message(self).await
+    }
 }
