eab8052d0f68054f3c090cdfbd14013dc3055ee1
Riley Wilton <30930649+wireboy5@users.noreply.github.com>
2023-10-03 20:19:10 +0000

    Oh, a reader needs to detect if it has read the front. That's rough.

--- src/types/broadcast.rs
+++ src/types/broadcast.rs
@@ -23,6 +23,7 @@ pub fn channel<T: Clone>() -> (Sender<T>, Receiver<T>) {
 
 
 /// The internal representation of a message on the broadcast channel
+#[derive(Debug)]
 struct BroadcastMessage<T: Clone> {
     /// The contents of the message
     message: T,
@@ -31,6 +32,7 @@ struct BroadcastMessage<T: Clone> {
 }
 
 /// This struct is held via an `Arc` by both receivers and senders.
+#[derive(Debug)]
 struct Inner<T: Clone> {
     /// The queue on which messages are stored
     queue: RwLock<VecDeque<BroadcastMessage<T>>>,
@@ -63,9 +65,7 @@ impl<T: Clone> Inner<T> {
         self.receivers.fetch_add(1, Ordering::Relaxed);
 
         Receiver {
-            waker: None,
             inner: self.clone(),
-            recv_fut: None,
         }
     }
     
@@ -142,14 +142,8 @@ impl<T: Clone> Sender<T> {
 
 
 /// The receive side of a broadcast channel
-#[pin_project::pin_project(PinnedDrop)]
 pub struct Receiver<T: Clone> {
-    /// The waker used by the receive future
-    waker: Option<Waker>,
     inner: Arc<Inner<T>>,
-    /// The currently receiving future
-    #[pin]
-    recv_fut: Option<Pin<Box<dyn Future<Output = Option<T>>>>>
 }
 
 impl<T: Clone + 'static> Receiver<T> {
@@ -159,12 +153,42 @@ impl<T: Clone + 'static> Receiver<T> {
         self.inner.clone().try_recv().await
     }
 
-    pub async fn recv(&mut self) -> T {
-        self.await
+    pub async fn recv(&self) -> T {
+        let mut recv = ReceiveFuture {
+            inner: self.inner.clone(),
+            recv_fut: None,
+        };
+        let fut = Pin::new(&mut recv);
+
+        fut.await
     }
 }
 
-impl<T: Clone + 'static> Future for Receiver<T> {
+impl<T: Clone> Drop for Receiver<T> {
+    fn drop(&mut self) {
+        // Decrement the number of receivers on drop.
+        self.inner.receivers.fetch_sub(1, Ordering::Relaxed);
+    }
+}
+impl<T: Clone> Clone for Receiver<T> {
+    fn clone(&self) -> Self {
+        self.inner.receivers.fetch_add(1, Ordering::Relaxed);
+
+        Self { inner: self.inner.clone() }
+    }
+}
+
+
+#[pin_project::pin_project]
+struct ReceiveFuture<T: Clone> {
+    /// A reference to the inner struct
+    inner: Arc<Inner<T>>,
+    /// The pinned future (Inner::try_recv) that we are currently waiting on
+    #[pin]
+    recv_fut: Option<Pin<Box<dyn Future<Output = Option<T>>>>>,
+}
+
+impl<T: Clone + 'static> Future for ReceiveFuture<T> {
     type Output = T;
 
     fn poll(mut self: core::pin::Pin<&mut Self>, cx: &mut core::task::Context<'_>) -> Poll<Self::Output> {
@@ -186,7 +210,6 @@ impl<T: Clone + 'static> Future for Receiver<T> {
 
         // If a message is ready, return it
         if let Some(m) = message {
-            self.waker = None;
             Poll::Ready(m)
         } else {
             // Otherwise, lock wakers as write and push the waker
@@ -208,22 +231,18 @@ impl<T: Clone + 'static> Future for Receiver<T> {
 }
 
 
-#[pin_project::pinned_drop]
-impl<T: Clone> PinnedDrop for Receiver<T> {
-    fn drop(self: Pin<&mut Self>) {
-        // Decrement the number of receivers on drop.
-        self.inner.receivers.fetch_sub(1, Ordering::Relaxed);
-    }
-}
+
 
 
 #[cfg(test)]
 mod tests {
+    use std::io::Write;
+
     use super::*;
 
     #[tokio::test]
     async fn test_receive_one() {
-        let (tx, mut rx) = channel::<i32>();
+        let (tx, rx) = channel::<i32>();
 
         // Make sure that a single value can be sent and received over the channel.
         tx.send(1).await;
@@ -238,6 +257,49 @@ mod tests {
         assert_eq!(rx.recv().await, 2);
         assert_eq!(rx.recv().await, 3);
         assert_eq!(rx.recv().await, 4);
+    }
+
+    #[tokio::test]
+    async fn test_receive_many() {
+        
+        let (tx, rx1) = channel::<i32>();
+        
+        // Clone the receiver
+        let rx2 = rx1.clone();
+        let rx3 = rx1.clone();
+        
+        // Send a single value
+        tx.send(1).await;
+
+        // Make sure that every receiver recieves said value
+        
+        assert_eq!(rx1.recv().await, 1);
+        assert_eq!(rx2.recv().await, 1);
+        assert_eq!(rx3.recv().await, 1);
+
+        // Send a couple values
+        tx.send(2).await;
+        tx.send(3).await;
+
+        // Ensure each receiver recieves the values
+        assert_eq!(rx1.recv().await, 2);
+        assert_eq!(rx2.recv().await, 2);
+        assert_eq!(rx3.recv().await, 2);
+        assert_eq!(rx1.recv().await, 3);
+        assert_eq!(rx2.recv().await, 3);
+        assert_eq!(rx3.recv().await, 3);
+
+        // Send two more
+        tx.send(4).await;
+        tx.send(5).await;
+
+        // Ensure each receiver may receive out of order
+        assert_eq!(rx1.recv().await, 4);
+        assert_eq!(rx1.recv().await, 5);
+        assert_eq!(rx2.recv().await, 4);
+        assert_eq!(rx2.recv().await, 5);
+        assert_eq!(rx3.recv().await, 4);
+        assert_eq!(rx3.recv().await, 5);
     }
 
 
