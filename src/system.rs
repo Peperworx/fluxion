f614c75dae45bac0f7af28aa1300605730d90117
Riley Wilton <30930649+wireboy5@users.noreply.github.com>
2023-10-05 17:12:34 +0000

    Foreign messages are currently todo.

--- src/system.rs
+++ src/system.rs
@@ -5,7 +5,7 @@ use core::{any::Any, marker::PhantomData};
 use alloc::{collections::BTreeMap, boxed::Box, sync::Arc};
 use maitake_sync::RwLock;
 
-use crate::{types::{actor::{ActorId, Actor}, params::{SystemParams, SupervisorGenerics}, executor::Executor}, supervisor::Supervisor, handle::{ActorHandle, LocalHandle}};
+use crate::{types::{actor::{ActorId, Actor}, params::{SystemParams, SupervisorGenerics}, executor::Executor, message::{MessageSender, Message}, Handle}, supervisor::Supervisor, handle::{ActorHandle, LocalHandle}};
 
 
 
@@ -67,5 +67,36 @@ impl<Params: SystemParams> System<Params> {
 
         // Return the handle
         Some(handle)
+    }
+
+    /// Retrieves a [`MessageSender`] for a given actor id and message. This will be a [`LocalHandle`]
+    /// if the actor is on the current system, but will be a [`ForeignHandle`] for foreign actors. [`None`] will
+    /// be returned if the target is the local system, but the actor is not found. Actors on foreign systems will
+    /// always be returned, as long as foreign messages are enabled. If they are not, then None will be returned
+    /// for all foreign actors.
+    pub async fn get<A: Handle<M>, M: Message>(&self, id: ActorId) -> Option<Box<dyn MessageSender<M>>> {
+
+        // If the system is the local system, find the actor
+        if id.get_system() == self.id.as_ref() || id.get_system() == "" {
+            
+            // Get the actor, returning None if it does not exist
+            let actor = self.actors.read().await.get(&id)?;
+
+            // Try to downcast to a concrete type
+            let actor: &LocalHandle<A> = actor.as_any().downcast_ref().as_ref()?;
+
+            // Clone and box the handle
+            let handle = Box::new(actor.clone());
+
+            // Return it
+            Some(handle)
+        } else {#[cfg(not(foreign))] {
+            // If foreign messages are disabled, return None
+            None
+        } #[cfg(foreign)] {
+
+
+            todo!()
+        }}
     }
 }
